2b3ac937d92b0c20ec213b11b72bb4cb1e46cc77
Reiki Aziz <reikiaziz30@gmail.com>
2025-09-24 15:14:12 +0700

    feat: update role user permission

---
+++ app/Models/Role.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Role extends Model
+{
+  protected $fillable = [
+    'name',
+    'display_name',
+    'description',
+    'is_active'
+  ];
+  protected $casts = [
+    'is_active' => 'boolean',
+  ];
+  public function users()
+  {
+    return $this->hasMany(User::class);
+  }
+
+  public function permissions()
+  {
+    return $this->belongsToMany(Permission::class, 'role_permission');
+  }
+  public function hasPermission(string $permission): bool
+  {
+    return $this->permissions()->where('name', $permission)->exists();
+  }
+  public function givePermission(Permission $permission)
+  {
+    return $this->permissions()->attach($permission);
+  }
+  public function removePermission(Permission $permission)
+  {
+    return $this->permissions()->detach($permission);
+  }
+}
